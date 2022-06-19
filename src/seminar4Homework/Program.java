ee0a423588b797333d43469f4e7316be681a9ccb
Albert <AccretionDiskThat@gmail.com>
2022-06-19 07:01:13 +0500

    fixed a bag

--- src/seminar4Homework/Program.java
+++ src/seminar4Homework/Program.java
@@ -3,10 +3,5 @@ package seminar4Homework;
 public class Program {
     public static void main(String[] args) {
 
-        System.out.println(product);
-
-        for (int i = 0; i < i; i++) {
-
-        }
     }
 }
