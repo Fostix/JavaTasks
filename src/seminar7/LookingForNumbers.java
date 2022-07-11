3fe204f54fb950ff1e13dbc2f6e94f4143be59f3
albert <accretiondiskthat@gmail.com>
2022-07-11 20:21:26 +0500

    filled an expression in an array

--- src/seminar7/LookingForNumbers.java
+++ src/seminar7/LookingForNumbers.java
@@ -1,14 +1,16 @@
 package seminar7;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 
 
 public class LookingForNumbers {
     private String expression;
-    private ArrayList<Character> firstNumber;
+    //private Deque<Character> firstNumber = new ArrayDeque<>();
     private char operation;
-    private ArrayList<Character> secondNumber;
-    private ArrayList<Character> thirdNumber;
+    //private ArrayList<Character> secondNumber = new ArrayList<>();
+    //private ArrayList<Character> thirdNumber = new ArrayList<>();
 
     public LookingForNumbers(String expression) {
         this.expression = expression;
@@ -39,24 +41,53 @@ public class LookingForNumbers {
     }
 
     public void distributor() {
-        expression.toCharArray();
 
         char[] symbols = expression.toCharArray();
-        // TODO: 11/07/2022 how write in pole
-        //System.out.printf("%s\n", symbols);
-        for (int i = 0; i < symbols.length; i++) {
-            boolean flag = true;
-            if(noSpace(symbols[i])) {
-                System.out.println("noSpace");
-            } else {
-                flag = false;
-            }
 
+//        ArrayList<ArrayList<Character>> numbers = new ArrayList<>();
+//
+//        ArrayList<Character> firstNumbers = new ArrayList<>();
+//        ArrayList<Character> secondNumbers = new ArrayList<>();
+//        ArrayList<Character> thirdNumbers = new ArrayList<>();
+
+        // char[][] numbers = new char[2][4];
+        char[] numbers = new char[30];
+
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < symbols.length; i++) {
+            // if space 0 and i++;
+            System.out.println(k);
+
+
+            if((noSpace(symbols[i]) && !checkOperation(symbols[i])) && symbols[i] != '=') {
+                //numbers[j][i] = symbols[i];
+                System.out.println(symbols);
+                numbers[k] = symbols[i];
+                k++;
+
+            } else if(checkOperation(symbols[i]) || symbols[i] == '=') { // (!checkOperation(symbols[i]) && symbols[i + 1] == '=') || checkOperation(symbols[i + 1])
+//                if (symbols[i - 1] == '=') {
+//                    j -= 10;
+//                }
+                j += 10;
+                k = j;
+                System.out.println(k);
+            }
             if(checkOperation(symbols[i])) {
                 this.operation = symbols[i];
             }
+            System.out.println(i + "   - i");
+            System.out.println(j + "   - j");
+            System.out.println(k + "   - k");
+
+            //System.out.println(numbers);
         }
 
+        for (var n: numbers) {
+            System.out.print(n);
+        }
+        System.out.println();
 
 
 
@@ -80,6 +111,13 @@ public class LookingForNumbers {
 
     @Override
     public String toString() {
+//        String forCheck = "";
+//        while (!firstNumber.isEmpty()) {
+//            forCheck = String.valueOf(firstNumber.spliterator());
+//            // firstNumber.
+//            firstNumber.removeFirst();
+//            System.out.println("here");
+//        }
         return String.format("%s, operation %c", this.expression, this.operation);
     }
 }
